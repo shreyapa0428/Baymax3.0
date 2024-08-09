70ff886f72b196a37894b7694bb96a7b22b9506f
Shreya <shreyapatki284@gmail.com>
2024-08-09 17:57:37 -0400

    First commit: Initial chatbot with question answering capability

---
+++ app.py
@@ -0,0 +1,53 @@
+from langchain_huggingface import HuggingFaceEndpoint
+import os
+import streamlit as st
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+os.environ['HUGGINGFACEHUB_API_TOKEN']="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"
+
+repo_id="openai-community/gpt2"
+# llm=HuggingFaceEndpoint(repo_id=repo_id, max_length=128, temperature=0.7,
+#                          token="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",max_new_tokens=500)
+# print(llm.invoke("What is machine learning"))
+
+
+def init_page():
+    st.set_page_config(
+        page_title="Baymax 3.0",
+    )
+    st.header("Your personal healthcare assistant")
+
+
+def select_llm(repo_id):
+    llm=HuggingFaceEndpoint(repo_id=repo_id, max_length=128, temperature=0.7,
+                         token="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",max_new_tokens=100, verbose=True)
+    return llm
+
+
+def main():
+    init_page()
+    
+    if "messages" not in st.session_state:
+        st.session_state.messages=[]
+    
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    if prompt:=st.chat_input("Say something"):
+        st.chat_message("user").markdown(prompt)
+        st.session_state.messages.append({"role":"user", "content":prompt})
+
+
+        llm=select_llm("openai-community/gpt2")
+        response=llm.invoke(prompt)
+
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        st.session_state.messages.append({"role":"assistant","content":response})
+
+
+if __name__=="__main__":
+    main()
+
+
+
